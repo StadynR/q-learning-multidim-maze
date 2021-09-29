032f50338c7e6d14d65e1238c862b3ced5e70ff7
StadynR <stadynr@gmail.com>
2021-09-29 16:17:10 -0500

    Upload odor and odor+dfs

---
+++ just_odor/MazeOdorGradient.cpp
@@ -0,0 +1,407 @@
+ /************************ Maze Odor Gradient.cpp **********************************
+
+Group Project of Artificial Intelligence 
+
+July 22 2021
+/*********************************************************************/ 
+//
+//  Students name:  Stadyn Román, Samantha Quintachala, Arianna Armijos, Washington Pijal
+//
+//------------------------------------------------------------------------------------------------
+// GLOBALS
+   
+#define  files 28
+#define  columns 28    //       size of the graphic maze   
+
+int b_flag;
+int stop;
+char dummy[1];
+//--
+char key, color;
+
+int x_agent, y_agent;
+int x_cheese, y_cheese;
+
+int x_plot_maze=30;
+int y_plot_maze=30;
+
+int square_size=50-33;        //
+int captured_color;
+
+
+//const int qSize = 6;
+const float gamma = 0.8;
+
+//int R[files][columns];
+int S[files][columns];    //
+int Q[files][columns];
+int Q_p[files][columns];
+
+int R[files][columns] = {
+    {0,-1,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1},
+    {0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1,0,-1,0,-1,0,-1},
+    {0,0,0,-1,0,-1,0,-1,0,-1,0,-1,0,0,0,0,0,-1,0,-1,0,-1,0,0,0,-1,0,-1},
+    {-1,-1,-1,-1,0,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1},
+    {0,0,0,0,0,0,0,-1,0,-1,0,0,0,-1,0,0,0,-1,0,-1,0,0,0,-1,0,0,0,-1,},
+    {0,-1,-1,-1,0,-1,0,-1,0,-1,-1,-1,-1,-1,0,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1},
+    {0,-1,0,0,0,-1,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,-1,0,-1},
+    {0,-1,-1,-1,0,-1,-1,-1,-1,-1,-1,-1,0,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,0,-1,0,-1},
+    {0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,-1,0,-1,0,-1},
+    {0,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1,-1,-1,-1,-1,-1,-1,0,-1,0,-1,0,-1},
+    {0,-1,0,-1,0,-1,0,-1,0,-1,0,0,0,-1,0,0,0,0,0,0,0,0,0,-1,0,-1,0,-1},
+    {0,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1,0,-1},
+    {0,-1,0,-1,0,0,0,0,0,0,0,-1,0,-1,0,-1,0,-1,0,-1,0,0,0,0,0,-1,0,-1},
+    {0,-1,-1,-1,-1,-1,-1,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1},
+    {0,0,0,-1,0,0,0,-1,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1},
+    {0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1,0,-1,-1,-1,-1,-1,0,-1,0,-1,-1,-1,0,-1},
+    {0,-1,0,-1,0,0,0,0,0,-1,0,0,0,-1,0,-1,0,0,0,-1,0,-1,0,0,0,0,0,-1},
+    {0,-1,-1,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1},
+    {0,0,0,-1,0,-1,0,-1,0,-1,0,-1,0,0,0,-1,0,-1,0,-1,0,0,0,0,0,-1,0,-1},
+    {0,-1,0,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1,0,-1},
+    {0,-1,0,-1,0,-1,0,-1,0,-1,0,0,0,-1,0,-1,0,-1,0,0,0,-1,0,0,0,0,0,-1},
+    {-1,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,0,-1},
+    {0,0,0,-1,0,0,0,-1,0,0,0,-1,0,-1,0,0,0,-1,0,-1,0,0,0,-1,0,-1,0,-1},
+    {0,-1,-1,-1,-1,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,-1,-1,0,-1,0,-1,0,-1,-1,-1,0,-1},
+    {0,-1,0,0,0,-1,0,-1,0,-1,0,-1,0,-1,0,0,0,-1,0,-1,0,-1,0,0,0,-1,0,-1},
+    {0,-1,0,-1,0,-1,0,-1,0,-1,-1,-1,0,-1,-1,-1,0,-1,0,-1,0,-1,0,-1,0,-1,0,-1},
+    {0,0,0,-1,0,0,0,-1,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,-1,0,0,0,-1},
+    {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1},
+        };
+
+int screen_image[files][columns];   //   presence  in pixels of the agent  and environment in the screen. Alter_ego  
+
+int currentState;   
+int episode;  
+
+int move_flag;
+
+int file_agent;    // file
+int column_agent;  // column
+
+int file_NPC;    // file
+int column_NPC;  // column
+
+int sensor[4];
+int MAX;
+int grad_pointer;
+
+int path[files*columns*3][2];
+int visited[files*columns][2];
+int cont;
+int stcont;
+int mov;
+
+int file_cheese, column_cheese;
+int player_captured_color;
+
+unsigned t0, t1;
+
+//----------------------------------------)))
+//    end globals
+//----------------------------------------)))
+#include <sstream>
+#include <stdlib.h>
+#include <graphics.h>
+#include <dos.h>
+#include <conio.h>
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+#include <iomanip.h>
+#include <windows.h>
+#include <ctime> 
+#include <fstream>
+
+#include "read_write_Q_matrix.h"
+#include "plot_maze_items.h"
+#include "Q_structures_lib.h"
+#include "all_possible_mazes.h"    
+
+
+//******************* variables globales ***************************************/
+using namespace std;
+
+
+//-----------------------------------------
+//    inicia el modo grafico             
+//-------------------------------------    
+void grafico(void)
+{
+    int    GraphDriver;  /* The Graphics device driver */
+    int    GraphMode;    /* The Graphics mode value */
+    int    ErrorCode;    /* Reports any graphics errors */
+   
+    initgraph(&GraphDriver, &GraphMode, "");
+
+    ErrorCode = graphresult();            /* Read result of initialization*/
+    if( ErrorCode != grOk )              /* Error occured during init    */
+    {-
+       printf(" Graphics System Error: %s", grapherrormsg( ErrorCode ) );
+       exit( 1 );
+    }
+}
+//-------------------------------------  
+void erase_screen(void)
+{
+    setcolor(LIGHTGRAY);
+    bar(0,0,2400,900);  
+}
+//-------------------------------------------------------
+
+
+void loadingBar()
+{
+
+    system("color 0A");
+
+    char a = 177, b = 219;
+  
+    printf("\n\n\n\n");
+    printf("\n\n\n\n\t\t\t\t\t + Loading...\n\n");
+    printf("\t\t\t\t\t");
+  
+    for (int i = 0; i < 26; i++)
+        printf("%c", a);
+
+    printf("\r");
+    printf("\t\t\t\t\t");
+
+    for (int i = 0; i < 26; i++) {
+        printf("%c", b);
+        Sleep(100);
+    }
+
+    system("cls");
+
+    printf("\t\t Maze Gradient Odor\n");
+
+
+}
+
+void Start_Message(void){
+  
+  moveto(0,250); 
+  settextstyle(4,0,4); 
+  setcolor(GREEN);
+  outtext("Welcome to the Maze Gradient Program ");
+
+
+  moveto(0,300); 
+  settextstyle(9,0,4); 
+  setcolor(GREEN);
+  outtext("Maximize the window size ");
+
+  //loadingBar();
+
+  erase_screen();
+
+  moveto(600,0); 
+  settextstyle(9,0,2); 
+  setcolor(WHITE);
+  outtext(" Maze Gradient Program  ");
+
+}
+//-------------------------------------------------------------
+void loop(void)  //                                                           loop  !!                        &&&/&&/&&/////
+{
+    //plot_maze();
+    //plot_olor_gradient();
+    //plot_agent();
+    //print_R();
+    //print_S();   //  gradiente de olor
+    
+    cout <<" ready to exploit-- " <<endl; 
+     
+    //getch();        
+    Q_explore();
+  
+}   
+//------------------------------------------------------
+void choose(void)
+{
+        //erase_screen();
+        
+    
+        moveto(40,420+100); 
+        settextstyle(4,0,4); 
+        setcolor(GREEN);
+        outtext(" Do you want to restart the environment? Press 'R' "); 
+        
+        moveto(20,470+100); 
+        settextstyle(4,0,4); 
+        setcolor(GREEN);
+        outtext(" Do you want change the cheese position? Press 'C' "); 
+        
+        moveto(50,520+100); 
+        settextstyle(4,0,4); 
+        setcolor(GREEN);
+        outtext(" Do you want change the maze? Press 'M' ");
+    
+        moveto(50,570+100); 
+        settextstyle(4,0,4); 
+        setcolor(GREEN);
+        outtext(" Do you want change the agent position? Press 'A' ");     
+        
+        do{
+            key=getch();
+            switch(key){
+                case 'R': case 'r':  //cambia todo el ambiente
+                {
+                    init_R();
+                    random_cheese();
+                    init_S();
+                    random_agent();
+                    plot_maze();
+                    plot_agent();
+                    plot_cheese();
+                } break;
+                case'C': case 'c':   // cambia solo el queso
+                {
+                    random_cheese();
+                    init_S();
+                    plot_maze();
+                    plot_agent();
+                    plot_cheese();
+                } break;
+                case'M': case 'm':    //cambia el maze
+                {
+                    init_R();
+                    init_S();
+                    plot_maze();
+                    plot_agent();
+                    plot_cheese();
+                    
+                } break;
+                case'A': case 'a':     //cambia el agente
+                {
+                    random_agent();
+                    plot_maze();
+                    plot_agent();
+                    plot_cheese();
+                } break;
+                case ' ':
+                {
+                    loop();
+                    random_agent();
+                }
+            }
+        }while(key!='x' && key!='X');
+}
+//===================================================================================================
+void main(void)
+{ 
+    int rand_seed=37;     // 26
+    int temp;
+    char str[10];  
+    grafico();
+    setcolor(LIGHTGRAY);
+    bar(0,0,2400,900);  
+
+    srand(rand_seed);  
+
+    randomize();
+
+    //file_agent=0;               
+    //column_agent=0;             
+
+    Start_Message();       // Messegae of welcome
+    
+    //init_R();                  //We get a random MAZE
+    //random_cheese();
+    file_cheese=14;   //  x_cheese:  coordinate x of agent in the screen    
+    column_cheese=12;   //  y_cheese:  coordinate y of agent in the screen  
+    
+    init_S();
+    random_agent();
+
+    x_agent=x_plot_maze;   //  x_agent:  coordinate x of agent in the screen    
+    y_agent=y_plot_maze;   //  y_agent:  coordinate y of agent in the screen  
+
+    print_R();
+    print_S();   //  gradiente de olor
+    plot_maze();
+    plot_agent();
+    plot_cheese();
+    //plot_olor_gradient();
+    
+    stop=0;
+    //loop();
+    choose();
+
+    /*
+    moveto(120,0);  // PRINT SCORE
+    setcolor(WHITE);
+    outtext("Score: ");
+    outtextxy(175,0,gcvt(score,6,str)); 
+    loop();
+
+    do {
+       //play_flag=0; 
+       key=getch();
+       switch (key) {
+
+            /*                        
+           case 'w':          {
+                               move_up();
+                               check_reward();
+                               plot_all(); 
+                               loop();                             
+                              } 
+           break;     
+           case ' ':          {          
+                               stop=stop^1;
+                               loop();
+                              }                                     
+           break; 
+                              
+            case 'R': case 'r':  {
+                                   init_R();
+                                   init_S();
+                                   random_agent();
+                                   plot_maze();
+                                   plot_agent();
+                                 }
+            break;                     
+            
+            default:
+                loop();
+         }
+   } while ((key!='x')&&(key!='X'));*/
+
+   closegraph();
+   clrscr();
+}
+
+//---------------------------------------
+/*
+BLACK        Â³  0  Â³ Yes Â³ Yes
+BLUE         Â³  1  Â³ Yes Â³ Yes
+GREEN        Â³  2  Â³ Yes Â³ Yes
+CYAN         Â³  3  Â³ Yes Â³ Yes
+RED          Â³  4  Â³ Yes Â³ Yes
+MAGENTA      Â³  5  Â³ Yes Â³ Yes
+BROWN        Â³  6  Â³ Yes Â³ Yes
+LIGHTGRAY    Â³  7  Â³ Yes Â³ Yes
+DARKGRAY     Â³  8  Â³ No  Â³ Yes
+LIGHTBLUE    Â³  9  Â³ No  Â³ Yes
+LIGHTGREEN   Â³ 10  Â³ No  Â³ Yes
+LIGHTCYAN    Â³ 11  Â³ No  Â³ Yes
+LIGHTRED     Â³ 12  Â³ No  Â³ Yes
+LIGHTMAGENTA Â³ 13  Â³ No  Â³ Yes
+YELLOW       Â³ 14  Â³ No  Â³ Yes
+WHITE        Â³ 15  Â³ No  Â³ Yes
+
+----------------------------------------------------------------
+     if(x<0)    { x=0; if( heat<100) heat++;}     
+     if(y<35)    { y=35; if( heat<100) heat++;}  
+     if(x>640) { x=640; if( heat<100) heat++;}  //  x>1200 : full pantalla 
+     if(y>470) { y=470; if( heat<100) heat++;}   // y>720 : full pantalla 
+         
+//--------------------------------------------------
+
+************************ basic_robot.cpp ***************************
+desarrollado usando Dev-C++ IDE V4.9.8.4 GNU / MinGW / GNU gcc
+                    Sc1 IDE / BCC5.5
+                    version orginal por Oscar Chang
+*********************************************************************/
+
---
+++ just_odor/Q_structures_lib.h
@@ -0,0 +1,494 @@
+//===========================================================================
+//  DEFINICION DE ESTRUCTURAS Q 
+//========================================================================
+//----------------------------------------------------------------------------------------------------     
+void print_R(void)
+{
+ int i,j;
+    
+    printf("This is the R Matrix:\n \n");
+    for(i = 0; i < files; i++)
+        {
+        for(j = 0; j < columns; j++)
+            {
+            cout << setw(4) << R[i][j];   
+		} 
+        cout << "\n";
+	} 
+    cout << "\n";
+}
+//----------------------------------------------------------------------------------------------------     
+void print_Q(void)
+{
+ int i,j;
+    
+    printf("This is the Q Matrix:\n \n");
+    for(i = 0; i < files; i++)
+        {
+        for(j = 0; j < columns; j++)
+            {
+            cout << setw(4) << Q[i][j];   
+		} 
+        cout << "\n";
+	} 
+    cout << "\n";
+}
+//---------------------------------------------------------------------------------------------
+void print_S(void)
+{
+ int i,j;
+    
+    printf("This is the S Matrix:\n \n");
+    for(i = 0; i < files; i++)
+        {
+        for(j = 0; j < columns; j++)
+            {
+            cout << setw(4) << S[i][j];   
+		}
+        cout << "\n";
+	} 
+    cout << "\n";
+}
+//------------------------------------------------------------------------------------------------
+void init_S(void)
+{
+ float lambda=10;    //  costante de decaimiento con la distancia
+ //float N=1.0;         //   N : scent concentration in a given square
+ float step=0.01;     // cuanto se avanza en cada loop  
+ int k, distx, disty;
+ 
+   for (int i=0; i < files; i++)
+   for (int j=0; j < columns; j++)
+    S[i][j]=-1;
+    
+   for (int i=0; i < files; i++)
+   for (int j=0; j < columns; j++)
+    {
+     disty = i-file_cheese;
+     distx = j-column_cheese;
+     k = sqrt(distx*distx+disty*disty);   // distancia pitagoras a coordenadas (0,0)
+     if(R[i][j]!=-1) S[i][j]=100* exp(-lambda*step*k);  // concentracion % a esa distancia
+    }   
+    //   se consulta a la matriz R por lo tanto el gradiente sigue la ley de las paredes
+}
+//-------------------------------------------------------------
+void print_visited(){
+    cout << "Visited: ";
+    for(int i = 0; i <= cont; i++){
+        cout << "[" << visited[i][0] << ", " << visited[i][1] << "] ";
+    }
+    cout << endl;
+}
+//------------------------------------------------------------
+void print_path(){
+    cout << "Path: ";
+    for(int i = 0; i <= stcont; i++){
+        cout << "[" << path[i][0] << ", " << path[i][1] << "] ";
+    }
+    cout << endl;
+}
+//------------------------------------------------------------
+void clear_visited(){
+    for(int i = 0; i <= cont; i++){
+        visited[i][0] = -1;
+        visited[i][1] = -1;
+    }
+}
+//------------------------------------------------------------
+void clear_path(){
+    for(int i = 0; i <= stcont; i++){
+        path[i][0] = -1;
+        path[i][1] = -1;
+    }
+}
+//------------------------------------------------------------
+void move_left(void)
+{
+ int temp;
+    temp=getpixel(x_agent-square_size,y_agent);   // revisa cercania a la izquierda
+    if(temp!=BLACK) // movimiento válido
+    {  
+     x_agent=x_agent-square_size;
+     column_agent--;
+    }    
+}
+//--------------------------------------------------------------
+void move_right(void)
+{
+ int temp;   
+  temp=getpixel(x_agent+square_size,y_agent);   // revisa cercania a la derecha
+  if(temp!=BLACK)
+     {
+      x_agent=x_agent+square_size;
+      column_agent++; 
+     }  
+}
+//-------------------------------------------------------------
+void move_up(void)
+{
+ int temp;
+    temp=getpixel(x_agent,y_agent-square_size);   // revisa cercania arriba
+    if(temp!=BLACK)
+    {
+     y_agent=y_agent-square_size;
+     file_agent--;
+    }  
+}
+//-------------------------------------------------------------
+void move_down(void)
+{
+ int temp;
+   temp=getpixel(x_agent,y_agent+square_size);    // revisa cercania abajo
+   if(temp!=BLACK)
+   {
+    y_agent=y_agent+square_size; 
+    file_agent++;
+   }   
+}
+//-------------------------------------------------------------
+bool no_unvisited() 
+{ 
+    bool f = true;
+    
+    for(int i = 0; i < 4; i++){
+        if(sensor[i] != -1){
+            f = false; 
+            break;
+        }
+    }
+    
+    return f;
+}
+//------------------------------------------------------------
+bool already_visited(int row, int col) 
+{ 
+    bool f = false;
+    
+    for(int i = 0; i < cont; i++){
+        if(visited[i][0] == row && visited[i][1] == col){
+            f = true; 
+            break;
+        }
+    }
+    
+    return f;
+}
+//--------------------------------------------------------
+void search_for_MAX_unvisited(void) 
+{
+ int i,j, sum = 0, r, lim_l, lim_r;
+    
+    sensor[0]= S[file_agent][column_agent-1];    // lee  a izquierda
+    if(column_agent==0 || already_visited(file_agent, column_agent-1)) sensor[0]=-1;            // desborde a la izquierd  
+        
+    sensor[1]= S[file_agent][column_agent+1];    // lee a  derecha
+    if(column_agent==columns-1 || already_visited(file_agent, column_agent+1)) sensor[1]=-1;
+    
+    sensor[2]= S[file_agent-1][column_agent];    // lee arriba
+    if(file_agent==0 || already_visited(file_agent-1, column_agent)) sensor[2]=-1;
+    
+    sensor[3]= S[file_agent+1][column_agent];    // lee abjo
+    if(file_agent==files-1 || already_visited(file_agent+1, column_agent)) sensor[3]=-1;
+
+    //MAX=sensor[0];
+    //for(i=0;i<4;i++) if(sensor[i]>=MAX) {MAX=sensor[i];grad_pointer=i;}  // grad_pointer apunta al maximo valor
+    
+    if(no_unvisited()) MAX = -1;
+    else{
+        for(i=0;i<4;i++){
+        if(sensor[i] != -1)
+            sum+=sensor[i];
+        }
+        if(sum > 1) r = random(sum); else r = 0;
+        //cout << "suma: " << sum << " r: " << r << endl;
+        //getch();
+        lim_l = 0;
+        for(i=0;i<4;i++){ 
+            if(sensor[i]!=-1){
+                lim_r = sensor[i] + lim_l;
+                if(r >= lim_l && r <= lim_r){
+                    MAX=sensor[i];
+                    grad_pointer = i;
+                    break;
+                }
+                lim_l = lim_r;
+            }
+        }
+    }
+}
+//------------------------------------------------------------
+void search_for_MAX(void) 
+{
+ int i,j, sum=0, r, lim_l, lim_r;
+    
+    sensor[0]= S[file_agent][column_agent-1];    // lee  a izquierda
+    if(column_agent==0) sensor[0]=-1;            // desborde a la izquierd  
+        
+    sensor[1]= S[file_agent][column_agent+1];    // lee a  derecha
+    if(column_agent==columns-1) sensor[1]=-1;
+    
+    sensor[2]= S[file_agent-1][column_agent];    // lee arriba
+    if(file_agent==0) sensor[2]=-1;
+    
+    sensor[3]= S[file_agent+1][column_agent];    // lee abjo
+    if(file_agent==files-1) sensor[3]=-1;
+        
+    
+    for(i=0;i<4;i++){
+        if(sensor[i] != -1)
+            sum+=sensor[i];
+    }
+    //cout << "suma: " << sum << endl;
+    //getch();
+    
+    if(sum > 1) r = random(sum); else r = 0;
+    //cout << "suma: " << sum << " r: " << r << endl;
+    //getch();
+    lim_l = 0;
+    for(i=0;i<4;i++){ 
+        if(sensor[i]!=-1){
+            lim_r = sensor[i] + lim_l;
+            if(r >= lim_l && r <= lim_r){
+                //MAX=sensor[i];
+                grad_pointer = i;
+                break;
+            }
+            lim_l = lim_r;
+        }
+    }
+}
+//------------------------------------------------------------
+void search_for_MAX_Q(void) 
+{
+ int i,j, sum=0, r, lim_l, lim_r;
+    
+    sensor[0]= Q[file_agent][column_agent-1];    // lee  a izquierda
+    if(column_agent==0) sensor[0]=-1;            // desborde a la izquierd  
+        
+    sensor[1]= Q[file_agent][column_agent+1];    // lee a  derecha
+    if(column_agent==columns-1) sensor[1]=-1;
+    
+    sensor[2]= Q[file_agent-1][column_agent];    // lee arriba
+    if(file_agent==0) sensor[2]=-1;
+    
+    sensor[3]= Q[file_agent+1][column_agent];    // lee abjo
+    if(file_agent==files-1) sensor[3]=-1;
+    
+    MAX=sensor[0];
+    for(i=0;i<4;i++) if(sensor[i]>=MAX) {MAX=sensor[i];}  // grad_pointer apunta al maximo valor
+    //cout <<" MAX: "<< MAX <<endl; 
+}
+//------------------------------------------------------------
+void backtrack() 
+{ 
+    int temp;
+    
+    //cout << "bfile: " << file_agent << " bcol: " << column_agent << endl;
+    
+    temp = R[file_agent][column_agent-1];
+    if(column_agent == 0) temp = -1;
+    if(temp != -1 && file_agent == path[stcont][0] && column_agent-1 == path[stcont][1]){
+        //cout << "Backtrack left: " << temp << endl;
+        mov = 0;
+        return;
+    }
+    
+    temp = R[file_agent][column_agent+1];
+    if(column_agent == columns-1) temp = -1;
+    if(temp != -1 && file_agent == path[stcont][0] && column_agent+1 == path[stcont][1]){ 
+        //cout << "Backtrack right: " << temp << endl;
+        mov = 1;
+        return;
+    }
+    
+    temp = R[file_agent-1][column_agent];
+    if(file_agent == 0) temp = -1;
+    if(temp != -1 && file_agent-1 == path[stcont][0] && column_agent == path[stcont][1]){ 
+        //cout << "Backtrack up: " << temp << endl;
+        mov = 2;
+        return;
+    }
+    
+    temp = R[file_agent+1][column_agent];
+    if(file_agent == files-1) temp = -1;
+    if(temp != -1 && file_agent+1 == path[stcont][0] && column_agent == path[stcont][1]){ 
+        //cout << "Backtrack down: " << temp << endl;
+        mov = 3;
+        return;
+    }
+}
+//------------------------------------------------------------
+void DFS(){
+      
+     path[stcont][0] = file_agent;
+     path[stcont][1] = column_agent;
+     visited[cont][0] = file_agent;
+     visited[cont][1] = column_agent;    
+     //cout << "Stack pos: " << stcont << " , row: " << file_agent << " , col: " << column_agent << endl;
+     //print_visited();
+    
+     search_for_MAX();
+     //cout << "max: " << MAX << " mov: " << grad_pointer << endl;
+     //getch();
+     if (MAX == -1){
+         stcont--;
+         cont++;
+         backtrack();
+     }
+     else{
+         cont++;
+         stcont++;       
+         mov = grad_pointer;          // el agente se mueve a un nuevo estado
+         }
+} 
+//-----------------------------------
+void save_q(void){
+    int i, j;
+    for(i=0;i<files;i++){
+        for(j=0;j<columns;j++){
+            Q_p[i][j]=Q[i][j];
+        }
+    }    
+}
+//-------------------------------------------------------
+bool check_q(bool go){
+    int i, j;
+    go = false;
+    for(i=0;i<files;i++){
+        for(j=0;j<columns;j++){
+            if(Q[i][j]!=Q_p[i][j]){
+                go=true;
+                break;
+            }
+        }
+    }
+    return go;
+}
+//-------------------------------------------------------
+void random_agent(void){
+    int i, j;
+    do{
+        i = random(files);
+        j = random(columns);
+        if(R[i][j]==0 || R[i][j]==100){ //if it is not in the wall or in the 
+            file_agent=i;           
+            column_agent=j;  
+        }
+    }while(R[i][j]<0); 
+} 
+//---------------------------------------------------------
+
+void random_cheese(void){
+    int i, j;
+    do{
+        i = random(files);
+        j = random(columns);
+        if(R[i][j]==0 || R[i][j]==100){ //if it is not in the wall or in the 
+            file_cheese=i;           
+            column_cheese=j;  
+        }
+    }while(R[i][j]<0); 
+} 
+//---------------------------------------------------------
+void Q_explore(void)  
+{
+    int episode, step;
+    bool go=true;
+    
+    string datas;
+    
+     randomize(); 
+
+     //plot_maze();
+     plot_agent();
+     
+     clear_visited();
+     clear_path();
+     cont = 0;
+     stcont = 0;
+         
+     episode=1;             // 
+     t0=clock();
+    do{
+         save_q();//save previous Q
+         plot_maze();
+         plot_olor_gradient();
+         plot_agent();
+         print_Q();
+         //print_R();
+         //print_S();   //  gradiente de olor
+         step=0;
+         do
+          {
+           
+           //DFS();
+              
+           search_for_MAX();
+           mov = grad_pointer;
+            
+           plot_trail();              
+           if(mov==0) move_left();
+           if(mov==1) move_right(); 
+           if(mov==2) move_up();
+           if(mov==3) move_down();
+               
+           search_for_MAX_Q();
+           Q[file_agent][column_agent]= S[file_agent][column_agent]+ gamma*MAX;
+           
+           //plot_maze();
+           plot_agent();                     //  sensor captures color
+           if(kbhit())
+            {
+                key=getch();
+                if(key=='P' || key=='p')
+                        getch();
+            }
+           delay(31);
+           step++;
+         }while(captured_color!=YELLOW);     //hasta que capture recompensa maxima o haya tardado mucho
+         
+        t1 = clock();
+         
+        go = check_q(go); //ver si esta estable Q
+        if(go){
+            string str; // a variable of str data type
+            string strtim; // a variable of str data type
+            string strcc; // a variable of str data type
+    
+            stringstream ss; 
+            stringstream tt; 
+            stringstream cc;  
+            double time = (double(t1-t0)/CLOCKS_PER_SEC);
+      
+            ss << episode;  
+            ss >> str;  
+    
+            datas.append("episode: "+str+"\n");
+    
+            tt << time;  
+            tt >> strtim; 
+            datas.append("Execution Time: "+strtim+"\n");
+            
+            cc << step;  
+            cc >> strcc; 
+            datas.append("Ciclos: "+strcc+"\n\n");
+            
+            episode++;
+            cout<<datas<<endl;
+        }
+        random_agent();
+    
+    }while(go);  // Unitil Q matix is stablish     
+     
+    print_visited();
+    print_path();
+    plot_path();
+    print_S();   
+
+    salvar_Q();    
+    salvar_data(datas);   
+    
+    //cout <<" one problem solved-- " <<endl;    
+}  
+//--------------------------------------------------------
+
